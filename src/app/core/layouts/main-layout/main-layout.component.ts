e9137070c4f327fe34ac8c8fd61a0552f50dba4d
Mohammad A'bed <mdarabed98@gmail.com>
2025-07-02 16:25:37 +0300

    Added log in page

---
+++ src/app/core/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,13 @@
+// main-layout.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-main-layout',
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './main-layout.component.html' ,
+  styleUrls: ['./main-layout.component.scss'],
+})
+export class MainLayoutComponent {}
---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    const { email, password } = this.loginForm.value as { email: string; password: string };
+    console.log('Login with:', email, password);
+    // Call auth service here later
+  }
+}
